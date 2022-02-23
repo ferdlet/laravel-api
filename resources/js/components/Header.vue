e6ddd723ada8d71a01881106051138b5846a5c80
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-02-23 18:50:12 +0100

    home vue

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
     <header>
-
+        <h2>Boolpress</h2>
+        <nav>
+            <ul>
+                <li><a href="/admin/home">Dashboard</a></li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -11,5 +16,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+header {
+    max-width: 100%;
+    min-height: 100px;
+    padding: 10px 100px 10px 100px;
+    display: flex;
+    justify-content: space-between;
+    background: #aaa;
+    align-items: center;
+    nav {
+        ul {
+            list-style: none;
+        }
+    }
+}
 </style>
--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,15 +1,68 @@
 <template>
     <main>
+        <div class="container">
+        <h2>Lista posts</h2>
+        <ul class="posts">
+            <li class="post-container" v-for="post in posts" :key="post.id">
+                <h3>{{post.title}}</h3>
+                <p>{{post.content}}</p>
+                <div class="categories">
+                    <strong>Categoria: </strong>
+                    <span v-for="(category, index) in post.category_id" :key="index">{{category}}</span>
+                </div>
+            </li>
+        </ul>
 
+        </div>
     </main>
 </template>
 
 <script>
 export default {
     name: 'Main',
+    data() {
+        return {
+            posts: []
+        }
+    },
+    created() {
+        axios.get("/api/posts")
+            .then((response) => {
+                this.posts = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.container {
+    width: 1200px;
+    margin: 0 auto;
+    h2 {
+        text-align: center;
+        margin: 20px 0 20px 0;
+    }
+    .posts {
+        list-style: none;
+        .post-container {
+            background-color: #aaa;
+            border-radius: 15px;
+            padding: 15px;
+            margin-top: 20px;
+            h3 {
+                text-align: center;
+            }
+            p {
+                padding: 15px;
+            }
+            .categories {
+                padding: 15px;
+            }
+        }
+    }
 
+}
 </style>
